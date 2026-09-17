c1fb1f4a9770ecb1a327c9ff8c6393149dddb3d3
seyyedahmadmadani65-hue <seyyedahmad.madani65@gmail.com>
2026-09-17 23:33:54 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "al-sokhari-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
+  );
+});
